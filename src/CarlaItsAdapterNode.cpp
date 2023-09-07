4a4774de000eb64d22b764945ffcddfb5c3ab476
Guido Küppers <guido.kueppers@ika.rwth-aachen.de>
2023-09-07 16:11:36 +0200

    fix utm-zone calculation

--- src/CarlaItsAdapterNode.cpp
+++ src/CarlaItsAdapterNode.cpp
@@ -127,9 +127,9 @@ void ItsAdapter::worldInfoCallback(const cm::CarlaWorldInfo::ConstPtr &msg){
   request->origin_lat = origin_lat;
   request->origin_lon = origin_lon;
 
-  int utm_zone = std::ceil((origin_lat + 180.0)/6);
-  int center_lon = 6 * utm_zone - 183;
-  grid_convergence_ = atan(tan(origin_lon * M_PI / 180.0 - (double)center_lon * M_PI / 180.0) * sin(origin_lat * M_PI / 180.0));
+  int utm_zone = std::ceil((origin_lon + 180.0)/6);
+  double center_lon = 6.0 * (double)utm_zone - 183.0;
+  grid_convergence_ = atan(tan(origin_lon * M_PI / 180.0 - center_lon * M_PI / 180.0) * sin(origin_lat * M_PI / 180.0));
 
   // check if service is available and send request
   if (!client_->wait_for_service(std::chrono::seconds(1))) {
@@ -277,7 +277,7 @@ void ItsAdapter::odometryCallback(const nm::Odometry::ConstPtr &msg)
     {
       ROS_LOG_STREAM(WARN, "\tTranformation from 'map' to 'carla_map' is not available");
     
-      // transformation between map and carla_map is always 0
+      // translation between map and carla_map is always 0
       gm::TransformStamped map_carla_map_transform;
       map_carla_map_transform.header.stamp = this->get_clock()->now();
       map_carla_map_transform.header.frame_id = "carla_map";
@@ -288,6 +288,7 @@ void ItsAdapter::odometryCallback(const nm::Odometry::ConstPtr &msg)
       map_carla_map_transform.transform.translation.z = 0.0;
 
       tf2::Quaternion q;
+      // add yaw offset due to grid-convergence
       q.setRPY(0, 0, -grid_convergence_);
       map_carla_map_transform.transform.rotation.x = q.x();
       map_carla_map_transform.transform.rotation.y = q.y();
