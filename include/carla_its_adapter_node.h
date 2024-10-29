bba021d3834976f6d7e6cee3e680a23753e83e34
Christian Geller <christian.geller@ika.rwth-aachen.de>
2024-10-29 07:34:05 +0000

    Update CarlaItsAdapterNode.h

--- include/carla_its_adapter_node.h
+++ include/carla_its_adapter_node.h
@@ -20,7 +20,7 @@
 #include <trajectory_planning_msgs_utils/trajectory_access.hpp>
 
 // tf2
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <tf2_ros/transform_listener.h>
